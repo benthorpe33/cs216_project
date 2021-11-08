caf19ab35728acb21269268811c3afeb9599884a
kordet26 <69854648+kordet26@users.noreply.github.com>
2021-11-08 18:44:48 -0500

    Add files via upload
    updated

--- nba.py
+++ nba.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import seaborn as sns
 # %% import and clean data
 nba = {}
 
@@ -25,9 +26,11 @@ for year in range(2005, 2022):
     
     nba[year]["wpct"] = nba[year]["W"]/(nba[year]["W"]+nba[year]["L"])
     if (year == 2021):
-        nba[year]["vegas_odds"] =  nba[year]["W-L O/U"]/72
+        nba[year]["vegas_wpct"] =  nba[year]["W-L O/U"]/72
+    elif (year == 2012):
+        nba[year]["vegas_wpct"] =  nba[year]["W-L O/U"]/66
     else:
-        nba[year]["vegas_odds"] =  nba[year]["W-L O/U"]/82
+        nba[year]["vegas_wpct"] =  nba[year]["W-L O/U"]/82
     
     nba[year] = nba[year].drop(columns = ['Unnamed: 17_level_1',
                                           'Unnamed: 22_level_1',
@@ -37,9 +40,12 @@ for year in range(2005, 2022):
                          'SOS', 'SRS', 'ORtg', 'DRtg', 'NRtg', 'Pace', 'FTr',
                          '3PAr', 'TS%', 'OeFG%', 'OTOV%', 'ORB%', 'OFT/FGA', 
                          'DeFG%', 'DTOV%', 'DRB%', 'DFT/FGA', 'Arena', 'Attend.',
-                         'Attend./G', 'W-L O/U', 'wpct', 'vegas_odds']
+                         'Attend./G', 'W-L O/U', 'wpct', 'vegas_wpct']
     yr = [year] * 30
     nba[year]["year"] = yr
+    
+    nba[year]["adj_3PAr"] = nba[year]["3PAr"] - nba[year]["3PAr"].mean()
+    nba[year]["adj_TS%"] =  nba[year]["TS%"] - nba[year]["TS%"].mean()
     
 data = pd.concat([nba[2005], nba[2006], nba[2007], nba[2008], nba[2009],
                   nba[2010], nba[2011], nba[2012], nba[2013], nba[2014],
@@ -57,16 +63,33 @@ r_sq = lm.score(x, y)
 
 data["predict"] = lm.predict(x)
 
-df = pd.read_csv("216nba.csv")
+predict_by_year = data.groupby("year")["predict"].mean()
 
-predict_by_year = df.groupby("year")["predict"].mean()
+# %%
 
-# Code from Elliot
+threes = data.groupby("year").mean()[["3PAr", "TS%"]]
+sns.relplot(data = threes, x = "year", y = "3PAr")
+sns.relplot(data = threes, x = "year", y = "TS%")
+
+data["over"] = (data["wpct"] > data["vegas_wpct"])*1
+sns.relplot(data = data, x = "adj_3PAr", y = "vegas_wpct", hue = "over")
+
+data["high_3PAr"] = (data["adj_3PAr"] > 0)*1
+data["high_TS%"] = (data["adj_TS%"] > 0)*1
+data["high_total"] = (data["vegas_wpct"] > 0.5)*1
+data["old"] = (data["Age"] > data["Age"].mean())*1
+
+pivot = data.groupby(["high_3PAr", "high_total"]).agg(["mean", "sum","count"])["over"]
+print(pivot)
+
+#sns.relplot(data = data[data["Team"]=="Houston Rockets"], x = "year", y = "3PAr")
+
+# %% Code from Elliot
 
 #plot = sns.regplot(x = predict_by_year.index, y = predict_by_year.values, data = predict_by_year)
 
-df["Odds_Error"] = df["wpct"] - df["vegas_odds"]
+data["Odds_Error"] = data["wpct"] - data["vegas_wpct"]
 
-error_by_year = df.groupby("year")["Odds_Error"].mean()
+error_by_year = data.groupby("year")["Odds_Error"].mean()
 
 e_plot = sns.regplot(x = error_by_year.index, y = error_by_year.values, data = error_by_year)
